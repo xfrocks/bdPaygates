1dc4a68703c70f168ca86a64bb677a75d4bc191d
Dao Hoang Son <daohoangson@gmail.com>
2013-12-12 14:04:31 +0700

    added support for recurrence ([bd] Shop)

--- library/bdPaygate/bdShop/StockPricing.php
+++ library/bdPaygate/bdShop/StockPricing.php
@@ -15,6 +15,11 @@ class bdPaygate_bdShop_StockPricing extends bdShop_StockPricing_Abstract
 
 	public function generateHtml($amount, $currency, $comment, array $data, XenForo_View $view)
 	{
+		return $this->generateHtmlRecurrence(0, $amount, $currency, $comment, $data, $view);
+	}
+
+	public function generateHtmlRecurrence($days, $amount, $currency, $comment, array $data, XenForo_View $view)
+	{
 		$data[] = utf8_strtolower($currency);
 		$data[] = $amount;
 
@@ -28,9 +33,28 @@ class bdPaygate_bdShop_StockPricing extends bdShop_StockPricing_Abstract
 			$processors[$processorId] = bdPaygate_Processor_Abstract::create($processorClass);
 		}
 
-		return implode('', bdPaygate_Processor_Abstract::prepareForms($processors, $amount, $currency, $comment, $itemId, false, false, array(
-				bdPaygate_Processor_Abstract::EXTRA_RETURN_URL => XenForo_Link::buildPublicLink('full:shop/thanks')
-		)));
+		$recurringInterval = false;
+		$recurringUnit = false;
+		if ($days > 0)
+		{
+			if ($days % 360 == 0)
+			{
+				$recurringInterval = $days / 365;
+				$recurringUnit = bdPaygate_Processor_Abstract::RECURRING_UNIT_YEAR;
+			}
+			elseif ($days % 30 == 0)
+			{
+				$recurringInterval = $days / 30;
+				$recurringUnit = bdPaygate_Processor_Abstract::RECURRING_UNIT_MONTH;
+			}
+			else
+			{
+				$recurringInterval = $days;
+				$recurringUnit = bdPaygate_Processor_Abstract::RECURRING_UNIT_DAY;
+			}
+		}
+
+		return implode('', bdPaygate_Processor_Abstract::prepareForms($processors, $amount, $currency, $comment, $itemId, $recurringInterval, $recurringUnit, array(bdPaygate_Processor_Abstract::EXTRA_RETURN_URL => XenForo_Link::buildPublicLink('full:shop/thanks'))));
 	}
 
 	/**
